670510a08463d09862909c00b586ef1ee022a9f2
Miguel Huchim <miguelhuchim94@gmail.com>
2020-01-09 10:39:56 -0600

    Exam 2nd month similar homes.

--- blog/app/models/home.rb
+++ blog/app/models/home.rb
@@ -2,21 +2,24 @@ class Home
   include Mongoid::Document
   include Mongoid::Enum
 
-  before_validation :calculate_total
+  before_validation :set_total_amount
 
   field :price, type: Float
   field :extra_service, type: String
   field :total_amount, type: Float
   field :home_features, type: Hash, default: { garden: false, furnished: false, gym: false }
   enum :status, [:in_progress, :published, :rented]
+  field :location, type: Array, default: [21.019777, -89.632661]
+  field :master_home_id, type: String
 
   belongs_to :owner
   has_many :rents
 
-  validates :price, :extra_service, :total_amount, :home_features, presence: true
+  validates :price, :extra_service, :total_amount, :home_features, :status, :location, presence: true
 
   private
-   def calculate_total
-     self.total_amount = price.to_f + extra_service.to_f
-   end
+
+  def set_total_amount
+    self.total_amount = price.to_f + extra_service.to_f
+  end
 end
---
+++ blog/app/models/similar_home.rb
@@ -0,0 +1,24 @@
+class SimilarHome
+  private
+
+  def self.find_similar
+    owners = Owner.includes(:homes)
+
+    owners.map do |owner|
+      homes_per_owner = owner.homes.where(_status: 'published', home_master_id: nil)
+      
+      homes_per_owner.select do |home|
+        home_total_amount_size = homes_per_owner.pluck(:total_amount).count(home.total_amount)
+        home_location_size = homes_per_owner.pluck(:location).count(home.location)
+
+        similar_homes = home_total_amount_size > 1 && home_location_size > 1
+
+        if similar_homes
+          text_format = "#{home.owner.user.name} #{home.owner.user.last_name}, #{home.owner.user.email} "
+          text_format += "#{home.owner_id}, #{home.total_amount}, #{home.status}, #{home.location}"
+          puts text_format
+        end
+      end
+    end
+  end
+end
